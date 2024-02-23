ac9c4789e267c217dfb415903689d1433e4d8b17
uaiaYR <160743805+uaiaYR@users.noreply.github.com>
2024-02-24 02:14:29 +0800

    4

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,15 +3,15 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
-JM508727
-508727
+JM536587
+536587
 
 '''
 
 # 单独下载章节
 jm_photos = '''
-JM508727
-508727
+
+
 '''
 
 
